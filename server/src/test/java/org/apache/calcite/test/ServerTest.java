a7b415f352ed31ee416f1c72c7f942b502855520
Julian Hyde <jhyde@apache.org>
2018-02-28 18:10:35 -0800

    Windows

--- server/src/test/java/org/apache/calcite/test/ServerTest.java
+++ server/src/test/java/org/apache/calcite/test/ServerTest.java
@@ -18,6 +18,7 @@ package org.apache.calcite.test;
 
 import org.apache.calcite.config.CalciteConnectionProperty;
 import org.apache.calcite.sql.parser.ddl.SqlDdlParserImpl;
+import org.apache.calcite.util.Util;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -120,7 +121,7 @@ public class ServerTest {
             + "EnumerableTableModify(table=[[T]], operation=[INSERT], flattened=[false])\n"
             + "  EnumerableCalc(expr#0..1=[{inputs}], expr#2=[1], expr#3=[+($t1, $t2)], proj#0..1=[{exprs}], J=[$t3])\n"
             + "    EnumerableValues(tuples=[[{ 3, 4 }]])\n";
-        assertThat(r.getString(1), is(plan));
+        assertThat(Util.toLinux(r.getString(1)), is(plan));
         assertThat(r.next(), is(false));
       }
 
