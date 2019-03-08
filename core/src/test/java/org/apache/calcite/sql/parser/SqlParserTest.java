95f9ea2268b8a337ec329a8dc9300242b2e97d29
Julian Hyde <jhyde@apache.org>
2019-03-07 19:40:27 -0800

    add test (fixup)

--- core/src/test/java/org/apache/calcite/sql/parser/SqlParserTest.java
+++ core/src/test/java/org/apache/calcite/sql/parser/SqlParserTest.java
@@ -62,7 +62,6 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.SortedSet;
 import java.util.TreeSet;
-
 import javax.annotation.Nonnull;
 
 import static org.hamcrest.CoreMatchers.equalTo;
