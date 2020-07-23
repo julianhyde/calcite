616531c303e5b4f3901d0c37f9c975ec228cf595
Julian Hyde <jhyde@apache.org>
2020-07-22 21:25:01 -0700

    polygon-to-polygon join

--- core/src/main/java/org/apache/calcite/adapter/enumerable/RexToLixTranslator.java
+++ core/src/main/java/org/apache/calcite/adapter/enumerable/RexToLixTranslator.java
@@ -1268,18 +1268,8 @@ public class RexToLixTranslator implements RexVisitor<RexToLixTranslator.Result>
       }
       final RexToLixTranslator.InputGetter getter =
           correlates.apply(((RexCorrelVariable) target).getName());
-      Expression input = getter.field(
+      final Expression input = getter.field(
           list, fieldIndex, currentStorageType);
-/* TODO
-      switch (nullAs) {
-      case IS_NULL:
-        input = Expressions.equal(input, RexImpTable.NULL_EXPR);
-        break;
-      case IS_NOT_NULL:
-        input = Expressions.notEqual(input, RexImpTable.NULL_EXPR);
-        break;
-      }
-*/
       final Expression condition = checkNull(input);
       final ParameterExpression valueVariable =
           Expressions.parameter(input.getType(), list.newName("corInp_value"));
