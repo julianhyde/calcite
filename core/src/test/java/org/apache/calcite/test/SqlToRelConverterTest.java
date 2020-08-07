907e8ae417722e5aeb6f8448e7ab572e3edf7085
Julian Hyde <jhyde@apache.org>
2020-08-06 23:00:56 -0700

    add a test case for aggregate measures

--- core/src/test/java/org/apache/calcite/test/SqlToRelConverterTest.java
+++ core/src/test/java/org/apache/calcite/test/SqlToRelConverterTest.java
@@ -3954,6 +3954,20 @@ class SqlToRelConverterTest extends SqlToRelTestBase {
     sql(sql).ok();
   }
 
+  /** As {@link #testMeasure1()} but uses an aggregate measure. */
+  @Disabled
+  @Test void testMeasure3() {
+    final String sql = "select deptno, count_plus_10, min(job) as min_job\n"
+        + "from (\n"
+        + "  select deptno,\n"
+        + "    job,\n"
+        + "    count(*) + 10 as measure count_plus_10,\n"
+        + "    count_plus_10 + deptno as measure e2\n"
+        + "  from emp)\n"
+        + "group by deptno";
+    sql(sql).ok();
+  }
+
   /** Test case for:
    * <a href="https://issues.apache.org/jira/browse/CALCITE-3310">[CALCITE-3310]
    * Approximate and exact aggregate calls are recognized as the same
