1838139741583c794dac59bcb157f26394210968
Julian Hyde <jhyde@apache.org>
2021-03-29 19:11:05 -0700

    start [CALCITE-4559] Create 'interface RexRule', a modular rewrite rule for row-expressions

---
+++ core/src/main/java/org/apache/calcite/rex/RexRule.java
@@ -0,0 +1,122 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.calcite.rex;
+
+import org.apache.calcite.plan.RelRule;
+import org.apache.calcite.rel.type.RelDataTypeFactory;
+import org.apache.calcite.sql.SqlKind;
+import org.apache.calcite.sql.SqlOperator;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * Rule that transforms a row-expression.
+ *
+ * <p>The analog for relational expressions is {@link RelRule}.
+ *
+ * @see RexRuleProgram
+ */
+public abstract class RexRule {
+
+  /** Asks the rule to build a description of what operands it matches.
+   *
+   * <p>That description will be used by the {@link RexRuleProgram} to index
+   * its rules, calling only those rules that may match a given expression. */
+  public abstract Done describe(OperandBuilder b);
+
+  /** Applies this rule to an expression.
+   *
+   * <p>Returns the transformed expression, or {@code e} if the rule does not
+   * apply. */
+  public abstract RexNode apply(Context cx, RexNode e);
+
+  /** Context in which a RexRule is applied. */
+  interface Context {
+    RelDataTypeFactory typeFactory();
+    RexBuilder rexBuilder();
+  }
+
+  /** Function that creates an operand.
+   *
+   * @see OperandDetailBuilder#inputs(OperandTransform...) */
+  @FunctionalInterface
+  public interface OperandTransform extends Function<OperandBuilder, Done> {
+  }
+
+  /** Callback to create an operand.
+   *
+   * @see RexRule#describe */
+  public interface OperandBuilder {
+    /** Matches any operand. */
+    Done any();
+
+    /** Matches any operand that is not null. */
+    Done notNull();
+
+    /** Matches any {@link RexLiteral} operand. */
+    default Done isLiteral() {
+      return isLiteral(literal -> true);
+    }
+
+    /** Matches a {@link RexLiteral} operand that matches a given predicate. */
+    Done isLiteral(Predicate<RexLiteral> predicate);
+
+    /** Indicates that the rule matches a {@link RexNode} of a given
+     * {@link SqlKind}. The matched expression may or may not be a
+     * {@link RexCall}; if not a {@code RexCall}, it will have zero inputs. */
+    OperandDetailBuilder ofKind(SqlKind kind);
+
+    /** Indicates that the rule matches a {@link RexCall} to a given
+     * {@link SqlOperator} instance. */
+    OperandDetailBuilder callTo(SqlOperator operator);
+
+    /** Indicates that the RexRule matches a {@link RexCall} to an
+     * {@link SqlOperator} that is an instance of a given class
+     * (or sub-class). */
+    OperandBuilder callTo(Class<? extends SqlOperator> operatorClass);
+  }
+
+  /** Indicates that an operand is complete.
+   *
+   * @see OperandBuilder */
+  public interface Done {
+  }
+
+  /** Add details about an operand, such as its inputs. */
+  public interface OperandDetailBuilder {
+    /** Sets the predicate of this call. */
+    OperandDetailBuilder predicate(Predicate<? super RexCall> predicate);
+
+    /** Indicates that this operand has a single input. */
+    Done oneInput(OperandTransform transform);
+
+    /** Indicates that this operand has several inputs. */
+    Done inputs(OperandTransform... transforms);
+
+    /** Indicates that this operand has several overloads. */
+//    Done overloads(OperandDetailBuilder... detailBuilders);
+
+    /** Indicates that this operand takes any number or type of inputs. */
+    Done anyInputs();
+
+    /** Indicates that this operand takes no inputs. */
+    default Done noInputs() {
+      return inputs();
+    }
+  }
+}
