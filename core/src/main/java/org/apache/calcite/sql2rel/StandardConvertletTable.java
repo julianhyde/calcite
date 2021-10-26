1009f3f929972ec19850b0ac7fa5c3bd8ab75e6b
Julian Hyde <jhyde@apache.org>
2021-10-25 17:06:56 -0700

    Add MEASURE type (part 2)

--- core/src/main/java/org/apache/calcite/sql2rel/StandardConvertletTable.java
+++ core/src/main/java/org/apache/calcite/sql2rel/StandardConvertletTable.java
@@ -264,7 +264,7 @@ public class StandardConvertletTable extends ReflectiveConvertletTable {
     registerOp(SqlStdOperatorTable.AS,
         (cx, call) -> cx.convertExpression(call.operand(0)));
     // "MEASURE" has no effect, so expand "x AS MEASURE id" into "x".
-    registerOp(SqlStdOperatorTable.MEASURE,
+    registerOp(SqlInternalOperators.MEASURE,
         (cx, call) -> cx.convertExpression(call.operand(0)));
     // "SQRT(x)" is equivalent to "POWER(x, .5)"
     registerOp(SqlStdOperatorTable.SQRT,
