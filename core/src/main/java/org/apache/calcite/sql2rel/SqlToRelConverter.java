9867f1a63dd69c374c53e697371c086b99a5cb99
Julian Hyde <jhyde@apache.org>
2021-11-13 12:06:19 -0800

    Start ProjectSortMeasureRule
    Improve field names

--- core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
+++ core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
@@ -648,7 +648,8 @@ public class SqlToRelConverter {
           .project(relBuilder.fields()
               .stream()
               .map(this::measureToValue)
-              .collect(Util.toImmutableList()))
+              .collect(Util.toImmutableList()),
+              relBuilder.peek().getRowType().getFieldNames())
           .build();
     }
     return r;
