27b93f0187d32ab880e2aac59dd7ff0f7ced6297
Julian Hyde <jhyde@apache.org>
2022-01-27 16:36:58 -0800

    measure in non-aggregate query works

--- core/src/main/java/org/apache/calcite/rel/rules/ProjectMergeRule.java
+++ core/src/main/java/org/apache/calcite/rel/rules/ProjectMergeRule.java
@@ -175,7 +175,6 @@ public class ProjectMergeRule
     default Config withOperandFor(Class<? extends Project> projectClass) {
       return withOperandSupplier(b0 ->
           b0.operand(projectClass)
-              .predicate(RexUtil.M2V_FINDER::inProject)
               .oneInput(b1 ->
                   b1.operand(projectClass).anyInputs()))
           .as(Config.class);
