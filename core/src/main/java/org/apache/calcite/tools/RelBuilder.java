be0343ad50d49cc11fc14d68b0a79c1250f62bc5
Julian Hyde <jhyde@apache.org>
2022-02-11 18:12:27 -0800

    start [CALCITE-xxxx] In RelBuilder add method aggregateExtended, to allow aggregating complex expressions such as "1 + SUM(x + 2)"

--- core/src/main/java/org/apache/calcite/tools/RelBuilder.java
+++ core/src/main/java/org/apache/calcite/tools/RelBuilder.java
@@ -2489,6 +2489,68 @@ public class RelBuilder {
     return project(fields);
   }
 
+  /** Creates an {@link Aggregate} with a set of hybrid expressions represented
+   * as {@link RexNode}.
+   */
+  public RelBuilder aggregateRex(GroupKey groupKey, Iterable<RexNode> nodes) {
+    final GroupKeyImpl groupKeyImpl = (GroupKeyImpl) groupKey;
+    final AggBuilder aggBuilder = new AggBuilder(groupKeyImpl.nodes);
+    for (RexNode node : nodes) {
+      aggBuilder.add(node);
+    }
+    return aggregate(groupKey, aggBuilder.aggCalls)
+        .project(aggBuilder.postProjects);
+  }
+
+  private class AggBuilder {
+    final ImmutableList<RexNode> groupKeys;
+    final List<RexNode> postProjects = new ArrayList<>();
+    final List<AggCall> aggCalls = new ArrayList<>();
+
+    private AggBuilder(ImmutableList<RexNode> groupKeys) {
+      this.groupKeys = groupKeys;
+    }
+
+    /** Adds a node that may or may not contain an aggregate function. */
+    void add(RexNode node) {
+      if (true || RexUtil.findAggregate().contains(node)) {
+        postProjects.add(convert(node));
+      } else {
+        postProjects.add(node);
+      }
+    }
+
+    /** Adds a node that we know to contain an aggregate function, and returns
+     * an expression whose input row type is the output row type of the
+     * aggregate layer ({@link #groupKeys} and {@link #aggCalls}). */
+    private RexNode convert(RexNode node) {
+      final RexBuilder rexBuilder = cluster.getRexBuilder();
+      if (node instanceof RexCall) {
+        final RexCall call = (RexCall) node;
+        if (call.getOperator().isAggregator()) {
+          final AggCall aggCall =
+              aggregateCall((SqlAggFunction) call.op, call.operands);
+          final int i = groupKeys.size() + aggCalls.size();
+          aggCalls.add(aggCall);
+          return rexBuilder.makeInputRef(call.getType(), i);
+        } else {
+          final List<RexNode> operands = new ArrayList<>();
+          call.operands.forEach(operand ->
+              operands.add(convert(operand)));
+          return call.clone(call.type, operands);
+        }
+      } else if (node instanceof RexInputRef) {
+        final int j = groupKeys.indexOf(node);
+        if (j < 0) {
+          throw new IllegalArgumentException("not a group key: " + node);
+        }
+        return rexBuilder.makeInputRef(node.getType(), j);
+      } else {
+        return node;
+      }
+    }
+  }
+
   /** Finishes the implementation of {@link #aggregate} by creating an
    * {@link Aggregate} and pushing it onto the stack. */
   private RelBuilder aggregate_(ImmutableBitSet groupSet,
