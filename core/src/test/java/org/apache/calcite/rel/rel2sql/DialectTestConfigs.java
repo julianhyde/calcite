c1b4322532d9fe9e9db3bcca0cbb250343d75bd1
Julian Hyde <jhyde@apache.org>
2023-03-04 19:22:01 -0800

    Add enum DialectCode

--- core/src/test/java/org/apache/calcite/rel/rel2sql/DialectTestConfigs.java
+++ core/src/test/java/org/apache/calcite/rel/rel2sql/DialectTestConfigs.java
@@ -19,22 +19,26 @@ package org.apache.calcite.rel.rel2sql;
 import org.apache.calcite.config.NullCollation;
 import org.apache.calcite.rel.type.RelDataTypeSystemImpl;
 import org.apache.calcite.sql.SqlDialect;
+import org.apache.calcite.sql.dialect.HiveSqlDialect;
 import org.apache.calcite.sql.dialect.JethroDataSqlDialect;
 import org.apache.calcite.sql.dialect.MssqlSqlDialect;
 import org.apache.calcite.sql.dialect.MysqlSqlDialect;
 import org.apache.calcite.sql.dialect.OracleSqlDialect;
 import org.apache.calcite.sql.dialect.PostgresqlSqlDialect;
 import org.apache.calcite.sql.type.SqlTypeName;
+import org.apache.calcite.sql.validate.SqlConformance;
+import org.apache.calcite.sql.validate.SqlConformanceEnum;
 import org.apache.calcite.util.Util;
 
 import com.google.common.base.Suppliers;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Ordering;
 
-import java.util.function.Consumer;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
 import java.util.function.Supplier;
 
-import static org.apache.calcite.rel.rel2sql.DialectTestConfig.Dialect.of;
+import static org.apache.calcite.util.Util.first;
 
 /** Utilities for {@link DialectTestConfig}. */
 class DialectTestConfigs {
@@ -45,7 +49,9 @@ class DialectTestConfigs {
       Suppliers.memoize(() -> {
         final ImmutableList.Builder<DialectTestConfig.Dialect> b =
             ImmutableList.builder();
-        extracted(b::add);
+        for (DialectCode dialectCode : DialectCode.values()) {
+          b.add(dialectCode.toDialect());
+        }
         final ImmutableList<DialectTestConfig.Dialect> list = b.build();
         final Iterable<String> dialectNames =
             Util.transform(list, dialect -> dialect.name);
@@ -55,80 +61,114 @@ class DialectTestConfigs {
         return DialectTestConfig.of(list);
       })::get;
 
-  private static void extracted(Consumer<DialectTestConfig.Dialect> list) {
-    // The following list must be ordered by dialect name.
-    list.accept(of("bigquery", SqlDialect.DatabaseProduct.BIG_QUERY));
-    list.accept(of("calcite", SqlDialect.DatabaseProduct.CALCITE));
-    list.accept(of("clickhouse", SqlDialect.DatabaseProduct.CLICKHOUSE));
-    list.accept(of("db2", SqlDialect.DatabaseProduct.DB2));
-    list.accept(of("exasol", SqlDialect.DatabaseProduct.EXASOL));
-    list.accept(of("firebolt", SqlDialect.DatabaseProduct.FIREBOLT));
-    list.accept(of("hive", SqlDialect.DatabaseProduct.HIVE));
-    list.accept(of("hsqldb", SqlDialect.DatabaseProduct.HSQLDB));
-    list.accept(of("informix", SqlDialect.DatabaseProduct.INFORMIX));
-    list.accept(of("jethro", JETHRO_DIALECT_SUPPLIER.get()));
-    // MSSQL 2008 = 10.0, 2012 = 11.0, 2017 = 14.0
-    list.accept(of("mssql", mssqlDialect(14)));
 
-    final SqlDialect mysqlDialect =
-        SqlDialect.DatabaseProduct.MYSQL.getDialect();
-    list.accept(of("mysql", mysqlDialect));
-    final SqlDialect mysql8Dialect =
-        new SqlDialect(
-            MysqlSqlDialect.DEFAULT_CONTEXT.withDatabaseMajorVersion(8)
-                .withIdentifierQuoteString(mysqlDialect.quoteIdentifier("")
-                    .substring(0, 1))
-                .withNullCollation(mysqlDialect.getNullCollation()));
-    list.accept(of("mysql-8", mysql8Dialect));
-    list.accept(of("mysql-first", mysqlDialect(NullCollation.FIRST)));
-    list.accept(of("mysql-high", mysqlDialect(NullCollation.HIGH)));
-    list.accept(of("mysql-last", mysqlDialect(NullCollation.LAST)));
-
-    list.accept(of("oracle", SqlDialect.DatabaseProduct.ORACLE));
-    list.accept(of("oracle-modified", oracleDialect(512)));
-    list.accept(of("postgresql", SqlDialect.DatabaseProduct.POSTGRESQL));
-    list.accept(of("postgresql-modified", postgresqlDialect(256)));
-    list.accept(of("presto", SqlDialect.DatabaseProduct.PRESTO));
-    list.accept(of("redshift", SqlDialect.DatabaseProduct.REDSHIFT));
-    list.accept(of("snowflake", SqlDialect.DatabaseProduct.SNOWFLAKE));
-    list.accept(of("spark", SqlDialect.DatabaseProduct.SPARK));
-    list.accept(of("sybase", SqlDialect.DatabaseProduct.SYBASE));
-    list.accept(of("vertica", SqlDialect.DatabaseProduct.VERTICA));
+  @SuppressWarnings("SameParameterValue")
+  static HiveSqlDialect hiveDialect(int majorVersion, int minorVersion) {
+    return new HiveSqlDialect(HiveSqlDialect.DEFAULT_CONTEXT
+        .withDatabaseMajorVersion(majorVersion)
+        .withDatabaseMinorVersion(minorVersion)
+        .withNullCollation(NullCollation.LOW));
   }
 
-  static MysqlSqlDialect mysqlDialect(NullCollation nullCollation) {
-    return new MysqlSqlDialect(MysqlSqlDialect.DEFAULT_CONTEXT
-        .withNullCollation(nullCollation));
+  static SqlDialect mysqlDialect(@Nullable Integer majorVersion,
+      @Nullable NullCollation nullCollation) {
+      SqlDialect d = SqlDialect.DatabaseProduct.MYSQL.getDialect();
+    SqlDialect.Context context =
+        MysqlSqlDialect.DEFAULT_CONTEXT
+            .withIdentifierQuoteString(d.quoteIdentifier("").substring(0, 1))
+            .withNullCollation(first(nullCollation, d.getNullCollation()));
+    if (majorVersion != null) {
+      context = context.withDatabaseMajorVersion(majorVersion);
+    }
+    if (nullCollation == null) {
+      // Historically, the MYSQL_8 dialect used in tests was an instance of
+      // SqlDialect, not MysqlSqlDialect. Preserve that behavior for now.
+      return new SqlDialect(context);
+    }
+    return new MysqlSqlDialect(context);
   }
 
-  static SqlDialect oracleDialect(final int maxVarcharLength) {
-    return new OracleSqlDialect(OracleSqlDialect.DEFAULT_CONTEXT
-        .withDataTypeSystem(new RelDataTypeSystemImpl() {
-          @Override public int getMaxPrecision(SqlTypeName typeName) {
-            switch (typeName) {
-            case VARCHAR:
-              return maxVarcharLength;
-            default:
-              return super.getMaxPrecision(typeName);
-            }
+  static SqlDialect oracleDialect(final int majorVersion,
+      final int maxVarcharLength) {
+    SqlDialect.Context context = OracleSqlDialect.DEFAULT_CONTEXT;
+    if (maxVarcharLength >= 0) {
+      context = context.withDataTypeSystem(new RelDataTypeSystemImpl() {
+        @Override public int getMaxPrecision(SqlTypeName typeName) {
+          switch (typeName) {
+          case VARCHAR:
+            return maxVarcharLength;
+          default:
+            return super.getMaxPrecision(typeName);
           }
-        }));
+        }
+      });
+    }
+
+    if (majorVersion >= 0) {
+      final SqlDialect oracleDialect = OracleSqlDialect.DEFAULT;
+      context =
+          context.withDatabaseMajorVersion(majorVersion)
+              .withIdentifierQuoteString(oracleDialect.quoteIdentifier("")
+                  .substring(0, 1))
+              .withNullCollation(oracleDialect.getNullCollation());
+    }
+    return new OracleSqlDialect(context);
   }
 
-  static SqlDialect postgresqlDialect(final int maxVarcharLength) {
-    return new PostgresqlSqlDialect(PostgresqlSqlDialect.DEFAULT_CONTEXT
-        .withDataTypeSystem(new RelDataTypeSystemImpl() {
-          @Override public int getMaxPrecision(SqlTypeName typeName) {
-            switch (typeName) {
-            case VARCHAR:
-              return maxVarcharLength;
-            default:
-              return super.getMaxPrecision(typeName);
-            }
-          }
-        }));
+  static SqlDialect postgresqlDialect(final int maxVarcharLength,
+      final boolean modifyDecimal) {
+    SqlDialect.Context context = PostgresqlSqlDialect.DEFAULT_CONTEXT;
+    if (maxVarcharLength >= 0) {
+      context =
+          context
+              .withDataTypeSystem(new RelDataTypeSystemImpl() {
+                @Override public int getMaxPrecision(SqlTypeName typeName) {
+                  switch (typeName) {
+                  case VARCHAR:
+                    return maxVarcharLength;
+                  default:
+                    return super.getMaxPrecision(typeName);
+                  }
+                }
+              });
+    }
+    if (modifyDecimal) {
+      context =
+          context.withDataTypeSystem(
+              new RelDataTypeSystemImpl() {
+                @Override public int getMaxNumericScale() {
+                  return getMaxScale(SqlTypeName.DECIMAL);
+                }
+
+                @Override public int getMaxScale(SqlTypeName typeName) {
+                  switch (typeName) {
+                  case DECIMAL:
+                    return 10;
+                  default:
+                    return super.getMaxScale(typeName);
+                  }
+                }
+
+                @Override public int getMaxNumericPrecision() {
+                  return getMaxPrecision(SqlTypeName.DECIMAL);
+                }
+
+                @Override public int getMaxPrecision(SqlTypeName typeName) {
+                  switch (typeName) {
+                  case DECIMAL:
+                    return 39;
+                  default:
+                    return super.getMaxPrecision(typeName);
+                  }
+                }
+              });
+    }
+    return new PostgresqlSqlDialect(context);
   }
 
+  /** Creates a dialect for Microsoft SQL Server.
+   *
+   * <p>MSSQL 2008 has version 10.0, 2012 has 11.0, 2017 has 14.0. */
   static SqlDialect mssqlDialect(int majorVersion) {
     final SqlDialect mssqlDialect =
         SqlDialect.DatabaseProduct.MSSQL.getDialect();
@@ -137,6 +177,16 @@ class DialectTestConfigs {
         .withIdentifierQuoteString(mssqlDialect.quoteIdentifier("")
             .substring(0, 1))
         .withNullCollation(mssqlDialect.getNullCollation()));
+  }
+
+  /** Creates a dialect that doesn't treat integer literals in the ORDER BY as
+   * field references. */
+  static SqlDialect nonOrdinalDialect() {
+    return new SqlDialect(SqlDialect.EMPTY_CONTEXT) {
+      @Override public SqlConformance getConformance() {
+        return SqlConformanceEnum.STRICT_99;
+      }
+    };
   }
 
   static final Supplier<SqlDialect> JETHRO_DIALECT_SUPPLIER =
