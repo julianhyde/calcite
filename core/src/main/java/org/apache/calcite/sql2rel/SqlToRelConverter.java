ca04962fbafab9e9c4dcf7db950338dd5b95ff89
Julian Hyde <jhyde@apache.org>
2023-06-15 23:01:34 -0700

    Refactor: Move class AggConverter out of SqlToRelConverter

--- core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
+++ core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
@@ -145,7 +145,6 @@ import org.apache.calcite.sql.SqlWithItem;
 import org.apache.calcite.sql.fun.SqlCase;
 import org.apache.calcite.sql.fun.SqlInOperator;
 import org.apache.calcite.sql.fun.SqlInternalOperators;
-import org.apache.calcite.sql.fun.SqlLibraryOperators;
 import org.apache.calcite.sql.fun.SqlQuantifyOperator;
 import org.apache.calcite.sql.fun.SqlRowOperator;
 import org.apache.calcite.sql.fun.SqlStdOperatorTable;
