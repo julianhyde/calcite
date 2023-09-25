f7bd4a3b9c1b277ea10eac38737fd3a84600d455
Julian Hyde <jhyde@apache.org>
2023-09-24 23:21:44 -0700

    Convert PairList and ImmutablePairList into interfaces
    In PairList, add left(int), right(int), transform2,
    addAll.
    
    Optimize representation of ImmutablePairList. There are
    dedicated classes for size = 0 and 1; and we use a backing
    array if size is 2 or more.

--- core/src/main/java/org/apache/calcite/runtime/ImmutablePairList.java
+++ core/src/main/java/org/apache/calcite/runtime/ImmutablePairList.java
@@ -16,62 +16,99 @@
  */
 package org.apache.calcite.runtime;
 
-import com.google.common.collect.ImmutableList;
-
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import static org.apache.calcite.linq4j.Nullness.castNonNull;
+
+import static java.util.Objects.requireNonNull;
 
 /** Immutable list of pairs.
  *
  * @param <T> First type
  * @param <U> Second type
  */
-public class ImmutablePairList<T, U> extends PairList<T, U> {
-  private static final ImmutablePairList<Object, Object> EMPTY =
-      new ImmutablePairList<>(ImmutableList.of());
-
-  @SuppressWarnings({"unchecked", "rawtypes"})
-  ImmutablePairList(ImmutableList<Object> list) {
-    super((ImmutableList) list);
-  }
+public interface ImmutablePairList<T, U> extends PairList<T, U> {
 
   /** Creates an empty ImmutablePairList. */
   @SuppressWarnings("unchecked")
-  public static <T, U> ImmutablePairList<T, U> of() {
-    return (ImmutablePairList<T, U>) EMPTY;
+  static <T, U> ImmutablePairList<T, U> of() {
+    return (ImmutablePairList<T, U>) PairLists.EMPTY;
   }
 
   /** Creates a singleton ImmutablePairList. */
-  public static <T, U> ImmutablePairList<T, U> of(@NonNull T t, @NonNull U u) {
-    return new ImmutablePairList<>(ImmutableList.of(t, u));
+  static <T, U> ImmutablePairList<T, U> of(T t, U u) {
+    return new PairLists.SingletonImmutablePairList<>(t, u);
   }
 
+  /** Creates an ImmutablePairList with one or more entries. */
+  static <T, U> PairList<T, U> copyOf(T t, U u, Object... rest) {
+    checkArgument(rest.length % 2 == 0, "even number");
+    if (rest.length == 0) {
+      return new PairLists.SingletonImmutablePairList<>(t, u);
+    }
+    Object[] elements = new Object[rest.length + 2];
+    elements[0] = requireNonNull(t, "t");
+    elements[1] = requireNonNull(u, "u");
+    System.arraycopy(rest, 0, elements, 2, rest.length);
+    return new PairLists.ArrayImmutablePairList<>(elements);
+  }
+
+  /** Creates an ImmutablePairList whose contents are a copy of a given
+   * collection. */
   @SuppressWarnings("unchecked")
-  public static <T, U> ImmutablePairList<T, U> copyOf(
-      Iterable<? extends Map.Entry<@NonNull T, @NonNull U>> iterable) {
+  static <@NonNull T, @NonNull U> ImmutablePairList<T, U> copyOf(
+      Iterable<? extends Map.Entry<T, U>> iterable) {
+    // Every PairList - mutable and immutable - knows how to quickly make
+    // itself immutable.
     if (iterable instanceof PairList) {
       return ((PairList<T, U>) iterable).immutable();
     }
-    ImmutableList.Builder<Object> builder = ImmutableList.builder();
-    iterable.forEach(p -> {
-      builder.add(p.getKey());
-      builder.add(p.getValue());
+
+    // If it's a collection, we know its size, and therefore can create an
+    // array directly, without an intermediate ArrayList.
+    if (iterable instanceof Collection) {
+      final Collection<? extends Map.Entry<T, U>> collection =
+          (Collection<? extends Map.Entry<T, U>>) iterable;
+      switch (collection.size()) {
+      case 0:
+        return of();
+
+      case 1:
+        // Use of iterator is suboptimal. If we knew this was a list we could
+        // call get(0), but the special case doesn't seem worth the effort.
+        final Map.Entry<T, U> entry = iterable.iterator().next();
+        return of(entry.getKey(), entry.getValue());
+
+      default:
+        Object[] elements = new Object[2 * collection.size()];
+        int i = 0;
+        for (Map.Entry<T, U> entry2 : iterable) {
+          elements[i++] = castNonNull(entry2.getKey());
+          elements[i++] = castNonNull(entry2.getValue());
+        }
+        return new PairLists.ArrayImmutablePairList<>(elements);
+      }
+    }
+
+    // Not a collection, so we don't know its size in advance.
+    final List<Object> list = new ArrayList<>();
+    iterable.forEach(entry -> {
+      list.add(castNonNull(entry.getKey()));
+      list.add(castNonNull(entry.getValue()));
     });
-    return new ImmutablePairList<>(builder.build());
+    return PairLists.immutableBackedBy(list);
   }
 
-  @Override public ImmutablePairList<T, U> immutable() {
+  @Override default ImmutablePairList<T, U> immutable() {
     return this;
   }
 
-  @Override public ImmutablePairList<T, U> subList(int fromIndex, int toIndex) {
-    return new ImmutablePairList<>(
-        ((ImmutableList<Object>) list).subList(fromIndex * 2, toIndex * 2));
-  }
-
-  @Deprecated // will always throw
-  @Override public void reverse() {
-    throw new UnsupportedOperationException();
-  }
+  @Override ImmutablePairList<T, U> subList(int fromIndex, int toIndex);
 }
