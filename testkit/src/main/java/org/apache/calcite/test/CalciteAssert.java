7f3a1a2bf540c2cee57b126df92aac72519ee199
Bertil Chapuis <bchapuis@gmail.com>
2024-02-03 10:33:09 +0100

    Fix the registration of the geometry schema

--- testkit/src/main/java/org/apache/calcite/test/CalciteAssert.java
+++ testkit/src/main/java/org/apache/calcite/test/CalciteAssert.java
@@ -41,7 +41,12 @@ import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.rel.type.RelDataTypeImpl;
 import org.apache.calcite.rel.type.RelProtoDataType;
-import org.apache.calcite.runtime.*;
+import org.apache.calcite.runtime.AccumOperation;
+import org.apache.calcite.runtime.CalciteException;
+import org.apache.calcite.runtime.CollectOperation;
+import org.apache.calcite.runtime.Hook;
+import org.apache.calcite.runtime.SpatialTypeFunctions;
+import org.apache.calcite.runtime.UnionOperation;
 import org.apache.calcite.schema.Schema;
 import org.apache.calcite.schema.SchemaPlus;
 import org.apache.calcite.schema.SchemaVersion;
@@ -49,7 +54,12 @@ import org.apache.calcite.schema.Statistic;
 import org.apache.calcite.schema.Table;
 import org.apache.calcite.schema.TableFunction;
 import org.apache.calcite.schema.Wrapper;
-import org.apache.calcite.schema.impl.*;
+import org.apache.calcite.schema.impl.AbstractSchema;
+import org.apache.calcite.schema.impl.AbstractTable;
+import org.apache.calcite.schema.impl.AggregateFunctionImpl;
+import org.apache.calcite.schema.impl.TableFunctionImpl;
+import org.apache.calcite.schema.impl.ViewTable;
+import org.apache.calcite.schema.impl.ViewTableMacro;
 import org.apache.calcite.sql.SqlCall;
 import org.apache.calcite.sql.SqlDialect;
 import org.apache.calcite.sql.SqlExplainLevel;
@@ -856,11 +866,14 @@ public class CalciteAssert {
           SpatialTypeFunctions.class.getName(), "*", true);
       ModelHandler.addFunctions(rootSchema, null, emptyPath,
           SqlSpatialTypeFunctions.class.getName(), "*", true);
+
       rootSchema.add("ST_UNION", AggregateFunctionImpl.create(UnionOperation.class));
       rootSchema.add("ST_ACCUM", AggregateFunctionImpl.create(AccumOperation.class));
       rootSchema.add("ST_COLLECT", AggregateFunctionImpl.create(CollectOperation.class));
+
       final SchemaPlus s =
-          rootSchema.add(schema.schemaName, new AbstractSchema());
+          rootSchema.add(schema.schemaName, new ReflectiveSchema(new GeometrySchema()));
+
       ModelHandler.addFunctions(s, "countries", emptyPath,
           CountriesTableFunction.class.getName(), null, false);
       final String sql = "select * from table(\"countries\"(true))";
@@ -886,7 +899,7 @@ public class CalciteAssert {
           ViewTable.viewMacro(rootSchema, sql3,
               ImmutableList.of("GEO"), emptyPath, false);
       s.add("parks", viewMacro3);
-      rootSchema.add(schema.schemaName, new ReflectiveSchema(new GeometrySchema()));
+
       return s;
     case HR:
       return rootSchema.add(schema.schemaName,
